b234cfc72fcde4771d6a2ad4a5e713a4b99fcef0
alaattin-duzdere <aleykedu@gmail.com>
2025-09-04 14:02:42 +0300

    Added roles in UserDetails

--- src/main/java/com/example/gallerist/model/User.java
+++ src/main/java/com/example/gallerist/model/User.java
@@ -1,17 +1,19 @@
 package com.example.gallerist.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.example.gallerist.enums.Role;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "user")
@@ -27,8 +29,19 @@ public class User extends BaseEntity implements UserDetails {
     @Column(name ="password")
     public String password;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id")
+    )
+    @Column(name = "role")
+    private List<Role> roles ;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .toList();
     }
 }
